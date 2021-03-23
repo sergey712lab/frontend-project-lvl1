624f98c8631219f0a6202eb6ca4ee7e13dd1a3bc
Sergei <Sergei@MacBook-Pro-Sergei.local>
2021-03-23 19:28:15 +0300

    bin mv

--- src/cli.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-
-const cli = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  return console.log(`Hello, ${userName}!`);
-};
-export default cli;
