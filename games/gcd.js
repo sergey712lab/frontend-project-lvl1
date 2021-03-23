f971bb296049848b3df06443d18e9e9e0a115dad
Sergei <Sergei@MacBook-Pro-Sergei.local>
2021-03-23 16:21:38 +0300

    fix style

--- games/gcd.js
+++ games/gcd.js
@@ -1,17 +1,17 @@
-import { cons } from '@hexlet/pairs'
-import random from '../libs/random.js'
-import playGame from '../index.js'
+import { cons } from '@hexlet/pairs';
+import random from '../libs/random.js';
+import playGame from '../index.js';
 
-const getGcd = (a, b) => (!b ? a : getGcd(b, a % b))
+const getGcd = (a, b) => (!b ? a : getGcd(b, a % b));
 
-const max = 100
-const description = 'Find the greatest common divisor of given numbers.'
+const max = 100;
+const description = 'Find the greatest common divisor of given numbers.';
 const getQuestionAndCorrectAnswer = () => {
-  const num1 = random(1, max)
-  const num2 = random(1, max)
-  const question = `${num1} ${num2}`
-  const correctAnswer = String(getGcd(num1, num2))
-  return cons(question, correctAnswer)
-}
+  const num1 = random(1, max);
+  const num2 = random(1, max);
+  const question = `${num1} ${num2}`;
+  const correctAnswer = String(getGcd(num1, num2));
+  return cons(question, correctAnswer);
+};
 
-export default () => playGame(description, getQuestionAndCorrectAnswer)
+export default () => playGame(description, getQuestionAndCorrectAnswer);
