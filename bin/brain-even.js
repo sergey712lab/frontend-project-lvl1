160eec46ba8dbc7d27eef640375f3ba181cb8792
Sergei <Sergei@MacBook-Pro-Sergei.local>
2021-03-23 12:30:41 +0300

    fix style

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!')
 const userName = readlineSync.question('May I have your name? ')
