20f3c515185e0fe21a11ce51acc43637578c4cef
Sergei <Sergei@MacBook-Pro-Sergei.local>
2021-03-23 14:44:23 +0300

    fix style

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,34 +1,30 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync'
+import readlineSync from 'readline-sync';
 
-console.log('Welcome to the Brain Games!')
-const userName = readlineSync.question('May I have your name? ')
-console.log(console.log(`Hello, ${userName}!`))
-console.log('Answer "yes" if the number is even, otherwise answer "no".')
-const randomNumberRange = (min, max) => {
-  return Math.floor(Math.random() * (max - min) + min)
-}
-const isEven = (x) => {
-  return x % 2 === 0
-}
-let correctAnswer
-let wrongAnswer = false
+console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? ');
+console.log(console.log(`Hello, ${userName}!`));
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const randomNumberRange = (min, max) => Math.floor(Math.random() * (max - min) + min);
+const isEven = (x) => x % 2 === 0;
+let correctAnswer;
+let wrongAnswer = false;
 for (let i = 0; i < 3; i += 1) {
-  const num = randomNumberRange(0, 100)
+  const num = randomNumberRange(0, 100);
   if (isEven(num)) {
-    correctAnswer = 'yes'
+    correctAnswer = 'yes';
   } else {
-    correctAnswer = 'no'
+    correctAnswer = 'no';
   }
-  console.log(`Question: ${num}`)
-  const userAnswer = readlineSync.question('Your answer: ')
-  if (userAnswer === correctAnswer) console.log('Correct!')
+  console.log(`Question: ${num}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+  if (userAnswer === correctAnswer) console.log('Correct!');
   else {
-    console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}. \nLet's try again, ${userName}!`)
-    wrongAnswer = true
-    break
+    console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${correctAnswer}. \nLet's try again, ${userName}!`);
+    wrongAnswer = true;
+    break;
   }
 }
 if (wrongAnswer === false) {
-  console.log(`Congratulations, ${userName}!`)
+  console.log(`Congratulations, ${userName}!`);
 }
